eabbbc18fbc978ffd40db6d6c688f4960807f8f2
Saulo Aguiar <sauloaguiar89@gmail.com>
2015-11-13 16:46:42 -0600

    Adding tests to models using only ActiveRecord methods.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,11 @@
 class Person < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   has_many :occupants
   has_many :apartments, through: :occupants
+
+  def name
+    [first_name, last_name].join " "
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,13 +1,78 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Person do
-  it "has a valid factory"
 
-  it "is invalid without first name"
+  def create_person(attrs = {})
+    default_attrs = {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      phone: Faker::Number.number(10)
+    }
+    Person.create(default_attrs.merge(attrs))
+  end
 
-  it "is invalid without last name"
+  context "has a valid factory" do
+    let(:person) { create_person() }
+    it "should have a valid instance" do
+        expect(person).to be_valid
+    end
+    it "should get saved" do
+        expect(person.save).to be true
+    end
+  end
 
-  it "is invalid without email"
+  context "when first name is empty" do
+    let(:person) { create_person(first_name: nil) }
+    it "should not be valid" do
+      expect(person).to_not be_valid
+    end
+    it "should not get saved" do
+      expect(person.save).to be false
+    end
+  end
 
-  it "is returns a person's full name as a string"
+  context "when last name is empty" do
+    let(:person) { create_person(last_name: nil) }
+    it "should not be valid" do
+      expect(person).to_not be_valid
+    end
+    it "should not get saved" do
+      expect(person.save).to be false
+    end
+  end
+
+  context "when email is empty" do
+    let(:person) { create_person(email: nil) }
+    it "should not be valid" do
+      expect(person).to_not be_valid
+    end
+    it "should not get saved" do
+      expect(person.save).to be false
+    end
+  end
+
+  context "duplicated email address" do
+    it "should not be valid" do
+      expect(create_person(email: "test@email.com")).to be_valid
+      expect(create_person(email: "test@email.com")).to_not be_valid
+    end
+  end
+
+  context "invalid email address" do
+    it "doesn't accept invalid domain name" do
+      expect(create_person(email: "t@m,com")).to_not be_valid
+    end
+    it "doesn't accept invalid username" do
+      expect(create_person(email: "*&â@m,com")).to_not be_valid
+    end
+  end
+
+  context "accessing person's full name" do
+    it "should return as a string" do
+      person = create_person(first_name: "Saulo", last_name: "Aguiar")
+      expect(person.name).to eq "Saulo Aguiar"
+    end
+  end
+  
 end
