86a6d6a037f65d1d0a627f2df89952e82a635829
Saulo Aguiar <sauloaguiar89@gmail.com>
2015-12-01 18:11:03 -0600

    Adding id attribute to person_serializer.rb for /users endpoint

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -1,6 +1,6 @@
 class PersonSerializer < ActiveModel::Serializer
   root :user
-  attributes :first_name, :last_name, :email, :phone
+  attributes :id, :first_name, :last_name, :email, :phone
 
   has_many :apartments
   has_many :buildings
